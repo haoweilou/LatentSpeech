2310043b39c074f7e39cc4ebb73ed6286d6308fb
haoweilou <591193117@qq.com>
2024-08-30 20:46:38 +1000

    save

--- README.md
+++ README.md
@@ -2,6 +2,8 @@ scp dataset.py function.py model.py params.py pqmf.py trainvqae.py tts_config.py
 ssh haoweilou@drstrange.cse.unsw.edu.au
 scp dataset.py function.py model.py params.py pqmf.py trainvqae.py tts_config.py test.py haoweilou@drstrange.cse.unsw.edu.au:~/LatentSpeech/
 
+
+scp haoweilou@drstrange.cse.unsw.edu.au:/home/haoweilou/LatentSpeech/model/vqae_50.pth ./model/
 scp haoweilou@drstrange.cse.unsw.edu.au:/home/haoweilou/scratch/model/TTS_hidden/Alinger/tts_600.pth ./save/model
 scp haoweilou@drstrange.cse.unsw.edu.au:/home/haoweilou/scratch/model/TTS_hidden/Alinger/tts_wo_da_600.pth ./save/model
 scp haoweilou@drstrange.cse.unsw.edu.au:/home/haoweilou/scratch/model/TTS_hidden/Alinger/tts_Melspec_600.pth ./save/model
--- test.py
+++ test.py
@@ -4,10 +4,18 @@ import torch.nn.functional as F
 import torch.optim as optim
 from model import VQAE
 from params import params
+from function import loadModel,save_audio
+from dataset import BakerAudio,pad16
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-device = "cpu"
-vqae = VQAE(params).to(device)
+model = VQAE(params).to(device)
+model = loadModel(model,"vqae_50","./model/")
+dataset = BakerAudio(0,100,"L:/baker/")
+audio = dataset.audios[0]
+audio = pad16(audio).to(device)
+audio = audio.unsqueeze(0).unsqueeze(0)
+generate_audio,spec_loss,vq_loss = model(audio)
+save_audio(generate_audio[0].detach().cpu(),48000,"fake",)
+save_audio(audio[0].detach().cpu(),48000,"real",)
 
-audio = torch.rand((64,1,48000)).to(device)
-generate_audio,spec_loss,vq_loss = vqae(audio)
-print(spec_loss,vq_loss)
+print(audio.shape,generate_audio.shape)
+# print(spec_loss,vq_loss)
